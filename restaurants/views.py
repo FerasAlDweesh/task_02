663ae7ccaeb75ab1a688ba20b62efd0eb59f4ef8
Feras AlDweesh <ferasaldweesh@gmail.com>
2019-09-30 18:43:11 +0300

    first commit for task_02

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def function(request):
+    context = {
+        "msg": "Hello World!",
+    }
+    return render(request, "home_page.html", context)
